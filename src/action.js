1d44c74eeda8e19e70dde8b9c8664e9bd4cb6fa9
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-09 09:31:13 +0530

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,4 +1,6 @@
-export function addAppointment() {
-    return {};
+export function addAppointment(appointment) {
+    return {
+    type: 'ADD_APPOINTMENT',
+    payload: appointment
+    };
 }
-  
